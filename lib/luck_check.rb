c3571956bf3d5bb321d4ed0212643f80d6aca525
moof mayeda <moofmayeda@yahoo.com>
2014-07-24 17:03:52 -0700

    make code more readable

--- lib/luck_check.rb
+++ lib/luck_check.rb
@@ -1,4 +1,8 @@
 def luck_check(number)
   range = number.length / 2
-  number[/[0-9]+/] == number ? (eval(number[0,range].chars.join("+")) == eval(number[-range..-1].chars.join("+"))) : false
+  if number[/[0-9]+/] == number
+    (eval(number[0,range].chars.join("+")) == eval(number[-range..-1].chars.join("+")))
+  else
+    false
+  end
 end
